bfa63db8d0892abb81df336674f5c096d598f8b0
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-19 12:38:26 -0700

    set up nav and footer

--- template-application/src/App.jsx
+++ template-application/src/App.jsx
@@ -1,11 +1,42 @@
 import React, { useState } from "react"
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home, About, Projects, Contact, Error } from './components'
 import "./App.css"
 
   const App = () => {
+    const [activeNavItem , setActiveNavItem] = useState(" ")
+
+    const handleNavItemClick = (navItem) => {
+      setActiveNavItem(navItem)
+    }
       return (
         <>
+        <nav>
+        <Link
+          to="/"
+          onClick={() => handleNavItemClick("Home")}
+          className = {`nav-item ${activeNavItem === "Home" ? "active" : "" }`}
+          > Home
+        </Link>
+        <Link
+          to="/about"
+          onClick={() => handleNavItemClick("About")}
+          className = {`nav-item ${activeNavItem === "About" ? "active" : "" }`}
+          > About
+        </Link>
+        <Link
+          to="/projects"
+          onClick={() => handleNavItemClick("Projects")}
+          className = {`nav-item ${activeNavItem === "Projects" ? "active" : "" }`}
+          > Projects
+        </Link>
+        <Link
+          to="/contact"
+          onClick={() => handleNavItemClick("Contact")}
+          className = {`nav-item ${activeNavItem === "Contact" ? "active" : "" }`}
+          > Contact
+        </Link>
+        </nav>
           <Routes> 
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
@@ -13,6 +44,7 @@ import "./App.css"
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<Error />} />
           </Routes>
+          <footer className="sticky-footer">Footer</footer>
         </>
       )
   }
