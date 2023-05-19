eacfa62248df6a29792110dfa0e9b8baaa2b4d30
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-19 16:21:29 -0700

    created product page

--- template-application/src/components/Projects.jsx
+++ template-application/src/components/Projects.jsx
@@ -1,8 +1,34 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import "../App.css"
 
 const Projects = () => {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => setProjects(data))
+    }, [])
+
     return (
-        <div>Projects</div>
+        <div className="project-container">
+            <h2 className="product-header"> My Projects </h2>
+            <div className="projects">
+                {projects.map(project => (
+                    <div key={project.id} className="project">
+                        <img src={project.image} alt={project.title} />
+                        <div className="project-details"> 
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
+                                <div className="project-links">
+                                    <a href={project.url} target="_blank" rel="noopener norefferrer">WishList</a>
+                                    <a href={project.url} target="_blank" rel="noopener norefferrer">Buy</a>
+                                </div>
+                        </div>
+                    </div>
+                ))}
+            </div>  
+        </div>
     )
 }
 
