4b6934a837843eea2661f646619ba374c27a6644
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-19 12:27:06 -0700

    added routing and dependiences

---
+++ template-application/src/components/index.js
@@ -0,0 +1,7 @@
+import Home from './home';
+import About from './about';
+import Contact from './contact';
+import Projects from './projects';
+import Error from "./error"
+
+export { Home, About, Contact, Projects, Error };
